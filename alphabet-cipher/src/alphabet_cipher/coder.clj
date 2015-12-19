55047d5569702c51b698400f3be6b8cfca89790a
Milan Lempera <milanlempera@gmail.com>
2015-12-19 16:08:17 +0100

    alphabet cipher - encode solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,31 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet
+  (map char (range (int \a) (inc (int \z)))))
+
+(defn char->position
+  [character]
+  (- (int character) (int \a)))
+
+(defn encode-char
+  [key char]
+  (let [key-position (char->position key)
+        char-position (char->position char)
+        offset (+ key-position char-position)]
+    (->> alphabet
+         cycle
+         (drop offset)
+         first)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map encode-char (cycle keyword) message)))
+
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
+
 
